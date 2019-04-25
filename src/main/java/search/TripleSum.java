01c24fb1f32648422157bea975134ce10a73472c
Paulo Augusto Dacach Bichara <contato@paulobichara.org>
2019-04-25 14:14:16 -0300

    Best so far!!!!!

--- src/main/java/search/TripleSum.java
+++ src/main/java/search/TripleSum.java
@@ -4,13 +4,281 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.stream.IntStream;
 
 public class TripleSum {
+
+    static class Node {
+
+        int key;
+        int size;
+
+        Node parent;
+        Node leftChild;
+        Node rightChild;
+
+        Node(int key) {
+            this.key = key;
+            this.size = 1;
+        }
+
+        void setLeftChild(Node node) {
+            leftChild = node;
+            if (node != null) {
+                node.parent = this;
+            }
+        }
+
+        void setRightChild(Node node) {
+            rightChild = node;
+            if (node != null) {
+                node.parent = this;
+            }
+        }
+
+        void updateSize() {
+            int sizeLeft = leftChild == null ? 0 : leftChild.size;
+            int sizeRight = rightChild == null ? 0 : rightChild.size;
+            size = sizeLeft + sizeRight + 1;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (!(obj instanceof Node)) {
+                return false;
+            }
+            return this.key == ((Node)obj).key;
+        }
+    }
+
+    private interface SplayStrategy {
+        boolean splayIfApplicable(Node node);
+
+        default void updateGreatGrandparent(Node node, Node grandparent, Node greatGrandparent) {
+            if (greatGrandparent != null) {
+                if (grandparent.equals(greatGrandparent.leftChild)) {
+                    greatGrandparent.setLeftChild(node);
+                } else {
+                    greatGrandparent.setRightChild(node);
+                }
+            } else {
+                node.parent = null;
+            }
+        }
+    }
+
+    private static class ZigZigStrategy implements  SplayStrategy {
+
+        private boolean isLeftCase(Node node) {
+            return node.equals(node.parent.leftChild) && node.parent.equals(node.parent.parent.leftChild);
+        }
+
+        private boolean isRightCase(Node node) {
+            return node.equals(node.parent.rightChild) && node.parent.equals(node.parent.parent.rightChild);
+        }
+
+        @Override
+        public boolean splayIfApplicable(Node node) {
+            Node parent = node.parent;
+            Node grandparent = parent == null ? null : parent.parent;
+            if (parent == null || grandparent == null) {
+                return false;
+            }
+
+            Node greatGrandparent = grandparent.parent;
+
+            if (isLeftCase(node)) {
+                Node oldRight = node.rightChild;
+                Node oldParentRight = parent.rightChild;
+                node.setRightChild(parent);
+                parent.setLeftChild(oldRight);
+                parent.setRightChild(grandparent);
+                grandparent.setLeftChild(oldParentRight);
+            } else if (isRightCase(node)) {
+                Node oldLeft = node.leftChild;
+                Node oldParentLeft = parent.leftChild;
+                node.setLeftChild(parent);
+                parent.setRightChild(oldLeft);
+                parent.setLeftChild(grandparent);
+                grandparent.setRightChild(oldParentLeft);
+            } else {
+                return false;
+            }
+
+            updateGreatGrandparent(node, grandparent, greatGrandparent);
+
+            grandparent.updateSize();
+            parent.updateSize();
+            return true;
+        }
+    }
+
+    private static class ZigZagStrategy implements  SplayStrategy {
+
+        private boolean isLessThanCase(Node node) {
+            return node.equals(node.parent.rightChild) && node.parent.equals(node.parent.parent.leftChild);
+        }
+
+        private boolean isGreaterThanCase(Node node) {
+            return node.equals(node.parent.leftChild) && node.parent.equals(node.parent.parent.rightChild);
+        }
+
+        @Override
+        public boolean splayIfApplicable(Node node) {
+            Node parent = node.parent;
+            Node grandparent = parent == null ? null : parent.parent;
+            if (parent == null || grandparent == null) {
+                return false;
+            }
+
+            Node greatGrandparent = grandparent.parent;
+
+            Node oldRight = node.rightChild;
+            Node oldLeft = node.leftChild;
+
+            if (isLessThanCase(node)) {
+                node.setRightChild(grandparent);
+                node.setLeftChild(parent);
+                parent.setRightChild(oldLeft);
+                grandparent.setLeftChild(oldRight);
+            } else if (isGreaterThanCase(node)) {
+                node.setLeftChild(grandparent);
+                node.setRightChild(parent);
+                parent.setLeftChild(oldRight);
+                grandparent.setRightChild(oldLeft);
+            } else {
+                return false;
+            }
+
+            updateGreatGrandparent(node, grandparent, greatGrandparent);
+
+            parent.updateSize();
+            grandparent.updateSize();
+            return true;
+        }
+    }
+
+    private static class ZigStrategy implements  SplayStrategy {
+
+        @Override
+        public boolean splayIfApplicable(Node node) {
+            Node parent = node.parent;
+            if (parent == null || parent.parent != null) {
+                return false;
+            }
+
+            node.parent = null;
+
+            if (node.equals(parent.leftChild)) {
+                Node oldRight = node.rightChild;
+                node.setRightChild(parent);
+                parent.setLeftChild(oldRight);
+            } else {
+                Node oldLeft = node.leftChild;
+                node.setLeftChild(parent);
+                parent.setRightChild(oldLeft);
+            }
+
+            parent.updateSize();
+            return true;
+        }
+    }
+
+    static class SplayTree {
+        static final SplayStrategy[] STRATEGIES = new SplayStrategy[]{new ZigStrategy(), new ZigZagStrategy(), new ZigZigStrategy()};
+
+        Node root = null;
+
+        Node find(int key) {
+            return find(key, true);
+        }
+
+        private Node find(int key, boolean mustSplay) {
+            Node current = root;
+            Node parent = null;
+            while (current != null && current.key != key) {
+                parent = current;
+                if (key > current.key) {
+                    current = current.rightChild;
+                } else {
+                    current = current.leftChild;
+                }
+            }
+
+            Node node = current == null ? parent : current;
+            if (mustSplay && node != null) {
+                splay(node);
+            }
+            return node;
+        }
+
+        void insert(int key) {
+            if (root == null) {
+                root = new Node(key);
+            } else {
+                Node found = find(key, false);
+                if (found.key != key) {
+                    Node node = new Node(key);
+                    if (found.key > key) {
+                        found.setLeftChild(node);
+                    } else {
+                        found.setRightChild(node);
+                    }
+                    splay(node);
+                }
+            }
+        }
+
+        private void splay(Node node) {
+            while (node.parent != null) {
+                for (int index = 0; index < STRATEGIES.length && !STRATEGIES[index].splayIfApplicable(node); index++);
+            }
+            node.updateSize();
+            root = node;
+        }
+
+    }
+
+    static long triplets(int[] a, int[] b, int[] c) {
+        SplayTree treeA = createTree(a);
+        SplayTree treeC = createTree(c);
+        Map<Integer, Boolean> computedMap = new HashMap<>();
+
+        long numTriplets = 0;
+        for (int index = 0; index < b.length; index++) {
+            if (!computedMap.containsKey(b[index])) {
+                computedMap.put(b[index], true);
+                numTriplets += getPossibleCandidates(treeA, b[index]) * getPossibleCandidates(treeC, b[index]);
+            }
+        }
+        return numTriplets;
+    }
+
+    private static int getPossibleCandidates(SplayTree tree, int upperBound) {
+        Node found = tree.find(upperBound);
+        if (found != null) {
+            if (found.key > upperBound) {
+                return found.leftChild == null ? 0 : found.leftChild.size;
+            } else {
+                return found.leftChild == null ? 1 : found.leftChild.size + 1;
+            }
+        }
+        return 0;
+    }
+
+    private static SplayTree createTree(int[] array) {
+        SplayTree tree = new SplayTree();
+        IntStream.of(array).forEach(tree::insert);
+        return tree;
+    }
 
     private static class Triplet {
         int first;
@@ -32,36 +300,56 @@ public class TripleSum {
     }
 
     // Complete the triplets function below.
-    static long triplets(int[] a, int[] b, int[] c) {
-        return 0;
-    }
-
     static long tripletsNaive(int[] a, int[] b, int[] c) {
+        int[] distinctA = getDistinctSortedArray(a);
+        int[] distinctB = getDistinctSortedArray(b);
+        int[] distinctC = getDistinctSortedArray(c);
+
+        Arrays.sort(distinctA);
+        Arrays.sort(distinctB);
+        Arrays.sort(distinctC);
+
         Map<Integer, List<Triplet>> tripletsMap = new HashMap<>();
         int countTriplets = 0;
-        for (int indexA = 0; indexA < a.length; indexA++) {
-            for (int indexB = 0; indexB < b.length; indexB++) {
-                for (int indexC = 0; indexC < c.length; indexC++) {
-                    if (a[indexA] <= b[indexB] && b[indexB] >= c[indexC]) {
-                        int key = a[indexA] + b[indexB] + c[indexC];
-                        if (!tripletsMap.containsKey(key)) {
-                            tripletsMap.put(key, new ArrayList<>());
-                        }
+        int indexMinAInB = getSearchIndex(distinctB, distinctA[0]);
+        int indexMinCInB = getSearchIndex(distinctB, distinctC[0]);
 
-                        List<Triplet> triplets = tripletsMap.get(key);
-                        final int valueA = a[indexA], valueB = b[indexB], valueC = c[indexC];
-                        Optional<Triplet> optional = triplets.stream().filter(triplet -> triplet.match(valueA, valueB, valueC)).findAny();
-                        if (optional.isPresent()) {
-                            optional.get().occurrences++;
-                        } else {
-                            triplets.add(new Triplet(valueA, valueB, valueC));
-                            countTriplets++;
-                        }
+        for (int indexB = Math.max(indexMinAInB, indexMinCInB); indexB < distinctB.length; indexB++) {
+            final int valueB = distinctB[indexB];
+            for (int indexA = 0; indexA < distinctA.length && distinctA[indexA] <= distinctB[indexB]; indexA++) {
+                final int valueA = distinctA[indexA];
+                for (int indexC = 0; indexC < distinctC.length && distinctC[indexC] <= distinctB[indexB]; indexC++) {
+                    final int valueC = distinctC[indexC];
+                    int key = distinctA[indexA] + distinctB[indexB] + distinctC[indexC];
+                    if (!tripletsMap.containsKey(key)) {
+                        tripletsMap.put(key, new ArrayList<>());
+                    }
+                    List<Triplet> triplets = tripletsMap.get(key);
+                    Optional<Triplet> optional = triplets.stream().filter(triplet -> triplet.match(valueA, valueB, valueC)).findAny();
+                    if (optional.isPresent()) {
+                        optional.get().occurrences++;
+                    } else {
+                        triplets.add(new Triplet(valueA, valueB, valueC));
+                        countTriplets++;
                     }
                 }
             }
         }
         return countTriplets;
+    }
+
+    private static int[] getDistinctSortedArray(int[] array) {
+        Set<Integer> set = new LinkedHashSet<>();
+        IntStream.of(array).forEach(set::add);
+        return set.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    private static int getSearchIndex(int[] array, int key) {
+        int resultIndex = Arrays.binarySearch(array, key);
+        if (resultIndex < 0) {
+            resultIndex = Math.abs(resultIndex + 1);
+        }
+        return resultIndex;
     }
 
     private static final Scanner scanner = new Scanner(System.in);
---
+++ src/test/java/search/TripleSumTest.java
@@ -0,0 +1,63 @@
+package search;
+
+import java.util.Random;
+import java.util.stream.IntStream;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TripleSumTest {
+    private static final Random RANDOM = new Random();
+
+    private static final int MIN_ARRAY_SIZE = 1;
+    private static final int MAX_ARRAY_SIZE = 5;
+
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 10;
+
+    private static final String MESSAGE = "Failed testing input: \n";
+
+    @Test
+    public void testSamples() {
+        assertInput(new int[] { 1, 3, 5 }, new int[] { 2, 3 }, new int[] { 1, 2, 3 });
+        assertInput(new int[] { 1, 4, 5 }, new int[] { 2, 3, 3 }, new int[] { 1, 2, 3 });
+        assertInput(new int[] { 1, 3, 5, 7 }, new int[] { 5, 7, 9 }, new int[] { 7, 9, 11, 13 });
+    }
+
+    private void assertInput(int[] array1, int[] array2, int[] array3) {
+        Assert.assertEquals(MESSAGE + inputToString(array1, array2, array3),
+                TripleSum.tripletsNaive(array1, array2, array3), TripleSum.triplets(array1, array2, array3));
+    }
+
+    @Test
+    public void stressTest() {
+        long startTime = System.nanoTime();
+        double duration = 0;
+
+        while (duration < 60) {
+            int[] array1 = getRandomArray();
+            int[] array2 = getRandomArray();
+            int[] array3 = getRandomArray();
+            try {
+                assertInput(array1, array2, array3);
+            } catch (Exception e) {
+                Assert.fail(MESSAGE + inputToString(array1, array2, array3));
+            }
+
+            duration = (System.nanoTime() - startTime) / 1_000_000_000.0;
+        }
+    }
+
+    private int[] getRandomArray() {
+        return RANDOM.ints(RANDOM.nextInt((MAX_ARRAY_SIZE + 1 - MIN_ARRAY_SIZE)) + MIN_ARRAY_SIZE,
+                MIN_NUMBER, MAX_NUMBER).toArray();
+    }
+
+    private String inputToString(int[] array1, int[] array2, int[] array3) {
+        StringBuilder builder = new StringBuilder().append(array1.length).append(" ").append(array2.length).append(" ")
+                .append(array3.length).append("\n");
+        IntStream.of(array1).forEach(number -> builder.append(number).append(" "));
+        IntStream.of(array2).forEach(number -> builder.append(number).append(" "));
+        IntStream.of(array3).forEach(number -> builder.append(number).append(" "));
+        return builder.append("\n").toString();
+    }
+}
