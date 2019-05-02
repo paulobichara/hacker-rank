f39a0cd8361f271e379d8b9d367600eb5b69b2dd
Paulo Augusto Dacach Bichara <contato@paulobichara.org>
2019-05-02 15:28:35 -0300

    Challenge completed!!!!!

--- src/main/java/strings/SherlockAndValidString.java
+++ src/main/java/strings/SherlockAndValidString.java
@@ -5,6 +5,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Scanner;
 
